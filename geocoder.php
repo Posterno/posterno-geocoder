75d505afbd43ac5b57d98fdffe9aad8137f0d61e
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-03-23 18:11:33 +0000

    update composer config and ignore files

---
+++ geocoder.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Hooks the Geocoder to Posterno.
+ *
+ * @package     posterno-geocoder
+ * @copyright   Copyright (c) 2019, Pressmodo, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       0.1.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
